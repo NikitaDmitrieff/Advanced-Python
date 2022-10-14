cd1b92de074560d80f1a487b1a06935d4ef40037
Nikita Dmitrieff <nnd19@ic.ac.uk>
2022-10-14 14:59:33 +0100

    Added all my work

--- bucket_fill.py
+++ bucket_fill.py
@@ -102,22 +102,135 @@ def fill(image, seed_point):
                2 represents a filled pixel
     """
 
-    # TODO: Complete this function
-    return []
+    check_conditions(image, seed_point)
 
+    left_to_check = []
+    left_to_check = check_neighbours(image, list(seed_point), left_to_check)
+
+    while len(left_to_check) > 0:
+        for empty_pixel in left_to_check:
+            check_neighbours(image, empty_pixel, left_to_check)
+
+    return image
+
+
+def check_neighbours(image, seed_pixel, left_to_check):
+    """ Colours the pixel and looks for other pixels to colour next and later check their neighbours
+
+    Args:
+        image (list) : list of lists of 0 (unfilled pixel), 1 (boundary pixel) and 2 (filled pixel)
+        seed_pixel (list) : 2-element list representing the (row, col) coordinates of the seed point to start filling
+        left_to_check (list) : list of coordinates left to colour and check the neighbours for
+
+    Returns:
+        list : coordinates left to colour and check neighbours for, now updated
+    """
+   
+    neighbours = gather_neighbours(image, seed_pixel) #List of neighbours
+    colour_blank_pixel(image, seed_pixel) #Colour the pixel    
+
+    if seed_pixel in left_to_check:
+        left_to_check.remove(seed_pixel)
+
+    for neighbour in neighbours:
+        if check_if_outofbonds(image, neighbour) and check_if_coloured(image, neighbour) and neighbour not in left_to_check:
+            left_to_check.append(neighbour)
+
+    return left_to_check
+    
+
+def gather_neighbours(image, seed_pixel):
+    """ Gathers the coordinates of the neighbouring pixels for a given pixel
+    Args:
+        image (list) : list of lists of 0 (unfilled pixel), 1 (boundary pixel) and 2 (filled pixel)
+        seed_pixel (list) : 2-element list representing the (row, col) coordinates of the seed point to start filling
+
+    Returns:
+        list : coordinates of neighbouring pixels for the given seed_pixel
+    """
+
+    left = [seed_pixel[0] - 1, seed_pixel[1]]
+    right = [seed_pixel[0] + 1, seed_pixel[1]]
+    up = [seed_pixel[0], seed_pixel[1] - 1]
+    down = [seed_pixel[0], seed_pixel[1] + 1]
+
+    neighbours = [left, right, up, down]
+
+    return neighbours
+
+def check_conditions(image, seed_pixel):
+    """ Verifies that the coordinates of the seed point are:
+            - integers
+            - not borders
+            - within the image size
+            - seed point not a coloured pixel
+
+    Args:
+        image (list) : list of lists of 0 (unfilled pixel), 1 (boundary pixel) and 2 (filled pixel)
+        seed_pixel (list) : 2-element list representing the (row, col) coordinates of the seed point to start filling
+    """
+
+    if type(seed_pixel[0]) != int or type(seed_pixel[1]) != int or type(seed_pixel) != tuple:
+        raise ValueError("Coordinates must be integers not {type(seed_pixel[0])}")
+    elif 0 == seed_pixel[0] or seed_pixel[0] == len(image[0]) - 1 or 0 == seed_pixel[1] or seed_pixel[0] == len(image) - 1:
+        raise ValueError("Seed point should not be on a boundary pixel")
+    elif not (check_if_outofbonds(image, seed_pixel)):
+        raise ValueError("Seed point is outside of the image")
+    elif check_if_coloured(image, seed_pixel) == False:
+        raise ValueError("Seed point given is a coloured pixel: ", seed_pixel)
+	
+    return
+
+def check_if_outofbonds(image, seed_pixel):
+    """ Verifies that the seed pixel is inside the image
+    Args:
+        image (list) : list of lists of 0 (unfilled pixel), 1 (boundary pixel) and 2 (filled pixel)
+        seed_pixel (list) : 2-element list representing the (row, col) coordinates of the seed point to start filling
+
+    Returns:
+        bool 
+    """
+
+    return 0 <= seed_pixel[0] <= len(image[0]) - 1 and 0 <= seed_pixel[1] <= len(image) - 1
+
+def check_if_coloured(image, seed_pixel):
+    """ Verifies that the seed pixel is inside the image
+    Args:
+        image (list) : list of lists of 0 (unfilled pixel), 1 (boundary pixel) and 2 (filled pixel)
+        pixel (list) : 2-element list representing the (row, col) coordinates of the seed point to start filling
+
+    Returns:
+        bool 
+    """
+
+    return image[seed_pixel[0]][seed_pixel[1]] == 0
+
+def colour_blank_pixel(image, pixel):
+    """ Verifies that the seed pixel is inside the image
+    Args:
+        image (list) : list of lists of 0 (unfilled pixel), 1 (boundary pixel) and 2 (filled pixel)
+        pixel (list) : 2-element list representing the (row, col) coordinates of the seed point to start filling
+
+    Returns:
+        list: list of lists of 0 (unfilled pixel), 1 (boundary pixel) and 2 (filled pixel), with the pixel desired now filled
+    """
+
+    assert check_if_coloured(image, pixel), f"Attempted to colour a black pixel located at {pixel}"
+    image[pixel[0]][pixel[1]] = 2
+    
+    return image
 
 def example_fill():
-    image = load_image("data/bar.txt")
+    image = load_image("data/snake.txt")
 
     print("Before filling:")
     show_image(image)
 
-    image = fill(image=image, seed_point=(7, 3))
+    image = fill(image=image, seed_point=(10, 1))
 
     print("-" * 25)
     print("After filling:")
     show_image(image)
-
 
 if __name__ == '__main__':
     example_fill()
--- test_bucket_fill.py
+++ test_bucket_fill.py
@@ -1,7 +1,112 @@
 from bucket_fill import fill
+from bucket_fill import load_image
+from bucket_fill import show_image
 
 def test_pattern():
+    """ 
+    Main testing functions which initiates all four tests 
+    present and prints messages.
+    """
+
+    print("\nTesting the fill function...\n")
+
+    test_normal_functionning()
+    test_incorrect_input_type()
+    test_seed_pixel_already_black()
+
+    print("\n...end of testing.\n")
+
     pass
+
+def test_seed_pixel_already_black():
+    """Tests the case where a pixel given is already coloured.
+    The fill function should display a ValueError.
+    """
+    
+    image = load_image("data/test_cases.txt")
+    seed_point = (4, 21)
+
+    try:
+        fill(image, seed_point)
+        print("test_seed_pixel_already_black ...... failed")
+    except ValueError:
+        print("test_seed_pixel_already_black ...... passed")
+
+def test_incorrect_input_type():
+    """Tests the case where a seed point given is given in an incorrect
+    format.
+        - Test 1 : as a tuple of string
+        - Test 2 : as a tuple of float
+        - Test 3 : as a list of int
+    """
+
+    image = load_image("data/test_cases.txt")
+    seed_point = ("2", "3")
+
+    try:
+        fill(image, seed_point)
+        print("test_incorrect_input_type str ...... failed")
+    except ValueError:
+        print("test_incorrect_input_type str ...... passed")
+
+    image = load_image("data/test_cases.txt")
+    seed_point = (2.0, 3.0)
+
+    try:
+        fill(image, seed_point)
+        print("test_incorrect_input_type float .... failed")
+    except ValueError:
+        print("test_incorrect_input_type float .... passed")
+
+    image = load_image("data/test_cases.txt")
+    seed_point = [2, 3]
+
+    try:
+        fill(image, seed_point)
+        print("test_incorrect_input_type list ..... failed")
+    except ValueError:
+        print("test_incorrect_input_type list ..... passed")
+
+def test_normal_functionning():
+    """ Tests the bucket_fill function by comparing to a test of known result
+
+0 0 0 0 0 0
+1 0 1 1 1 0
+0 1 0 0 0 1
+0 0 1 1 1 0
+0 0 0 0 0 0
+
+    Using three different seed_points to colour the three different areas
+    of the above figure, the test_pattern() function checks for various cases.
+    """
+
+    image = load_image("data/test_cases.txt")
+
+    seed_point_1 = (22, 13)
+    seed_point_2 = (7, 15)
+    seed_point_3 = (5, 2)
+
+    expected_results = []
+
+    expected_results.append(load_image("data/test_1.txt"))
+    expected_results.append(load_image("data/test_2.txt"))
+    expected_results.append(load_image("data/test_3.txt"))
+
+    results = []
+
+    results.append(fill(image, seed_point_1))
+
+    image = load_image("data/test_cases.txt")
+    results.append(fill(image, seed_point_2))
+
+    image = load_image("data/test_cases.txt")
+    results.append(fill(image, seed_point_3))
+
+    if results == expected_results:
+        print("test_normal_functionning ........... passed")
+
+    else:
+        print("test_normal_functionning ........... failed")
 
 
 if __name__ == '__main__':
